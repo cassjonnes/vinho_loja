19c8f40c4ea4bb16e13315ac17055d15cc9f5ba1
cassjonnes <cass.mollona@gmail.com>
2024-01-21 12:07:36 -0300

    atualizar placar e unidades restantes

---
+++ dados.js
@@ -0,0 +1,66 @@
+let itens = [{
+    classe: 'vinho',
+    titulo: 'Casa Silva',
+    tipo: 'Tinto',
+    uva: 'Cabernet Sauvignon',
+    preco: 55,
+    unidades: 6
+},
+{
+    classe: 'vinho',
+    titulo: 'Casillero Del Diablo',
+    tipo: 'Tinto',
+    uva: 'Cabernet Sauvignon',
+    preco: 65,
+    unidades: 4
+},
+{
+    classe: 'vinho',
+    titulo: 'Granja União',
+    tipo: 'Tinto',
+    uva: 'Cabernet Sauvignon',
+    preco: 35,
+    unidades: 2
+},
+{
+    classe: 'vinho',
+    titulo: 'Reservado',
+    tipo: 'Tinto',
+    uva: 'Cabernet Sauvignon',
+    preco: 45,
+    unidades: 4
+},
+{
+    classe: 'vinho',
+    titulo: 'Trapiche',
+    tipo: 'Tinto',
+    uva: 'Cabernet Sauvignon',
+    preco: 60,
+    unidades: 3
+},
+{
+    classe: 'vinho',
+    titulo: 'Tierruca',
+    tipo: 'Branco',
+    uva: 'Sauvignon Blanc',
+    preco: 40,
+    unidades: 3
+},
+{
+    classe: 'espumante',
+    titulo: 'Garibaldi',
+    tipo: '',
+    uva: '',
+    preco: 35,
+    unidades: 8
+},
+{
+    classe: 'cerveja',
+    titulo: 'Pack de Heineken',
+    tipo: '',
+    uva: '',
+    preco: 50,
+    unidades: 2
+}];
+
+module.exports = itens;
